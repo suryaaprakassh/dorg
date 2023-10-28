3815e5775bdfc0572988525a31435a793fa2a754
surya prakash <surya0prakash@proton.me>
2023-10-28 07:34:21 +0530

    added default config

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-set -xe
+
+CONFIG_FILE="$HOME/.dorg.conf"
+
+TEMPLATE_DIR="./src/templates/template.conf"
 gcc -c src/parse.c -o src/parse.o
 gcc -o dorg src/parse.o src/main.c
 sudo mv dorg /usr/bin/
 echo "Installation Successful!"
+
+#default config
+
+read -p "Do you want to create config file in your home directory? (y/n): " choice
+
+echo "Nah gonna do it anyways!"
+
+if [ ! -f "$CONFIG_FILE" ]; then 
+  cp "$CONFIG_TEMPLATE_PATH" "$CONFIG_FILE"
+  echo "Default Config Created in Home Directory!"
+else
+  echo "Config File Already Exists!"
+fi
+
